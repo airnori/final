dc6408fa9620d9e88601227665737875791a85d2
airnori <a01041099808@gmail.com>
2023-11-27 19:24:01 +0900

    1234

--- jedol3AiFun.py
+++ jedol3AiFun.py
@@ -196,7 +196,7 @@ def vectorDB_create(vectorDB_folder):
 
 def ai_response( vectorDB_folder="", query="", token="", ai_mode="" ):
 
-    openai.api_key = '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'
+    openai.api_key = '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'
     client = OpenAI()
     chat_history=chatDB.query_history(token,ai_mode) # 기존 대화 내용
     # print("chat_history=",chat_history)
